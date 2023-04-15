b73d56c540dc66875d5ca25521a4920c99ba3f92
aliescont <aescontrela@gmail.com>
2023-04-15 14:09:33 +0200

    adding prefect blocks

--- prefect/blocks/make_docker_blocks.py
+++ prefect/blocks/make_docker_blocks.py
@@ -1,10 +1,9 @@
 from prefect.infrastructure.docker import DockerContainer
 
 docker_block = DockerContainer(
-    image="aliescont/prefect:steam",
+    image="aliescont/etl_prefect_steam:dezoomcamp",
     image_pull_policy = "ALWAYS",
     auto_remove=True,
-
 )
 
-docker_block.save("steam-test", overwrite=True)
+docker_block.save("etl-prefect-docker", overwrite=True)
